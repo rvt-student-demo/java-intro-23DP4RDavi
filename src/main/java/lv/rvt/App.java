18179fffd656a6c89ad313d732ad20857f74a43b
Roberts Dāvidsons <166591879+23DP4RDavi@users.noreply.github.com>
2024-09-13 11:26:33 +0000

    Uzdevums 0

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,9 +1,17 @@
 package lv.rvt;
 
+import java.util.*;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        // Make your Hello World programm!
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.nextLine();
+        String surname = scanner.nextLine();
+        String group = scanner.nextLine();
+
+        System.out.println("Students: " + name + " " + surname + ", grupa " + group);
     }
 }
