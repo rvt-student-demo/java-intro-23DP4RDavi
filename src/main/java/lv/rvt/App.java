44fa726c1b1982a6ddd49684dde6e4f0d4e79aac
Roberts Dāvidsons <166591879+23DP4RDavi@users.noreply.github.com>
2024-09-20 10:38:04 +0000

    Simple calculator fixed

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -18,7 +18,7 @@ public class App
         System.out.println(num1 + " - " + num2 + " = " + result2);
         double result3 = num1 * num2;
         System.out.println(num1 + " * " + num2 + " = " + result3);
-        double result4 = num1 / num2;
+        double result4 = (double)num1 / num2;
         System.out.println(num1 + " / " + num2 + " = " + result4);
 
     }
