b56296aa7387407058a3675811aa05ca08e4dd6c
Roberts Dāvidsons <166591879+23DP4RDavi@users.noreply.github.com>
2024-09-27 09:52:42 +0000

    Speeding Ticket

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,19 +7,10 @@ public class App
     public static void main( String[] args )
     {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Give the first number:");
-        double num1 = Double.valueOf(scanner.nextLine());
-        System.out.println("Give the second number:");
-        double num2 = Double.valueOf(scanner.nextLine());
-
-        double result1 = num1 + num2;
-        System.out.println(num1 + " + " + num2 + " = " + result1);
-        double result2 = num1 - num2;
-        System.out.println(num1 + " - " + num2 + " = " + result2);
-        double result3 = num1 * num2;
-        System.out.println(num1 + " * " + num2 + " = " + result3);
-        double result4 = (double)num1 / num2;
-        System.out.println(num1 + " / " + num2 + " = " + result4);
-
+        System.out.println("Give speed:");
+        double speed = Double.valueOf(scanner.nextLine());
+        if (speed > 120) {
+            System.out.println("Speeding ticket!");
+        }
     }
 }
