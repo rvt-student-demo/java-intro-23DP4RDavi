82f3e8ed2e2e7430e03ecdae0e2e0ec8efba5580
Roberts Dāvidsons <166591879+23DP4RDavi@users.noreply.github.com>
2024-10-10 11:07:07 +0000

    Number and sum of numbers

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,23 +8,25 @@ public class App
     {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Value of the gift?");
+        double sum = 0;
+        double amt = 0;
 
-        int tax = Integer.valueOf(scanner.nextLine());
+        while (true) {
+            System.out.println("Give a number: ");
+            double numb = Double.valueOf(scanner.nextLine());
+            if (numb == 0) {
+                System.out.println("Number of numbers: " + amt);
+                System.out.println("Sum of the numbers: " + sum);
+                break;
+            }
+            else{
+                sum = sum + numb;
+                amt = amt + 1;
+            }
+            
 
-        if(tax < 5000){
-            System.out.println("No tax!");
-        }else if(tax >= 5000 && tax < 25000){
-            System.out.println("Tax: "+((double)100+(tax-5000)*0.08));
-        }else if(tax >= 25000 && tax < 55000){
-            System.out.println("Tax: "+((double)1700+(tax-25000)*0.1));
-        }else if(tax >= 55000 && tax < 200000){
-            System.out.println("Tax: "+((double)4700+(tax-55000)*0.12));
-        }else if(tax >= 200000 && tax < 1000000){
-            System.out.println("Tax: "+((double)22100+(tax-200000)*0.15));
-        }else{
-            System.out.println("Tax: "+((double)142100+(tax-1000000)*0.17));
         }
+
     }
 }
 
