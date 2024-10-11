31128870e1800b0b71e4396184a0f83904a8b91f
Roberts Dāvidsons <166591879+23DP4RDavi@users.noreply.github.com>
2024-10-11 09:53:21 +0000

    Repeating, breaking and remembering

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,16 +4,38 @@ import java.util.*;
 
 public class App 
 {
-            public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Give a number:");
+    public static void main( String[] args )
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give numbers: ");
+        int sum = 0;
+        int count = 0;
+        double avg = 0;
+        int even = 0;
+        int odd = 0;
+        while (true) {
             int num = scanner.nextInt();
-
-            int fact = 1;
-            for (int i = 1; i <= num; i++) {
-                fact = i*fact;
+        
+            if (num == -1) {
+                System.out.println("Thx! Bye!");
+                System.out.println("Sum: " + sum);
+                System.out.println("Numbers: " + count);
+                avg = (double)sum / (double)count;
+                System.out.println("Average: " + avg);
+                break;
             }
-            System.out.println("Factorial: " + fact);
-        }
-    }
+            else{
+                sum = num + sum;
+                count = count + 1;
+                if (num % 2 == 0) {
+                    even = even + 1;
+                } else {
+                    odd = odd + 1;
+                }
+                continue;
+            }
 
+        }
+        
+    }
+}
