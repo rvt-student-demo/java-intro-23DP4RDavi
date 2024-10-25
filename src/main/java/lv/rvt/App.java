a750c717c91defba3491eacdda985dd074da4857
Roberts Dāvidsons <166591879+23DP4RDavi@users.noreply.github.com>
2024-10-25 10:26:09 +0000

    cristmass tree

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,65 +6,42 @@ public class App
 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter what a number of u want to print out:");
-        System.out.println("1: line");
-        System.out.println("2: square");
-        System.out.println("3: rectangle");
-        System.out.println("4: triangle");
-        int choice = scanner.nextInt();
-        if (choice == 1) {
-            System.out.println("How long should it be?");
-            int x = scanner.nextInt();
-            printStars(x);
+        System.out.println("What do you want to print? ");
+        System.out.println("1) triangle");
+        System.out.println("2) cristmass tree");
+        int selection = scanner.nextInt(); 
+        if (selection == 1) {
+            System.out.print("Enter the size of the triangle: ");
+            int size = scanner.nextInt();
+            printTriangle(size);
         }
-        else if (choice == 2) {
-            System.out.println("How wide/tall should it be?");
-            int x = scanner.nextInt();
-            printSquare(x);
-        }
-        else if (choice == 3) {
-            System.out.println("How wide should it be?");
-            int x = scanner.nextInt();
-            System.out.println("How high should it be?");
-            int y = scanner.nextInt();
-        }
-        else if (choice == 4) {
-            System.out.println("How tall should it be?");
-            int x = scanner.nextInt();
-            printTriangle(x);
-        }
-    }
-    public static void printStars(int number) {
-        for (int i = 0; i < number; i++) {
-            System.out.print("*");
-        }
-        System.out.println();
-    }
-    public static void printSquare(int size) {
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-    }
-    public static void printRectangle(int width, int height) {
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
+        else if (selection == 2) {
+            System.out.print("Enter the size of the Christmas tree: ");
+            int size = scanner.nextInt();
+            christmasTree(size);
         }
     }
     public static void printTriangle(int size) {
-        for (int i = 0; i < size; i++) {
-            for (int j = size - i - 1; j > 0; j--) {
-                System.out.println();
+        for (int i = 1; i <= size; i++) {
+            for (int j = size - i; j > 0; j--) {
+                System.out.print("");
             }
-            for (int k = 0; k <= i; k++) {
+            for (int k = 1; k <= i; k++) {
                 System.out.print("*");
             }
             System.out.println();
         }
+    }
+    public static void christmasTree(int size) {
+        for (int i = 1; i <= size; i++) {
+            for (int j = size - i; j > 0; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        size = size / 3;
     }
 }
