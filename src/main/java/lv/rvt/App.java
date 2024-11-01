5aefb439538d3eb26c5bfa3e082b2080a6ef93b7
Roberts Dāvidsons <166591879+23DP4RDavi@users.noreply.github.com>
2024-11-01 10:39:04 +0000

    Exercise 1 — Array Sum

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,44 +4,15 @@ import java.util.*;
 
 public class App 
 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("What do you want to print? ");
-        System.out.println("1) triangle");
-        System.out.println("2) cristmass tree");
-        int selection = scanner.nextInt(); 
-        if (selection == 1) {
-            System.out.print("Enter the size of the triangle: ");
-            int size = scanner.nextInt();
-            printTriangle(size);
-        }
-        else if (selection == 2) {
-            System.out.print("Enter the size of the Christmas tree: ");
-            int size = scanner.nextInt();
-            christmasTree(size);
-        }
+  public static void main(String[] args) {
+    
+    int[] val = {0, 1, 2, 3}; 
+        
+    int sum = 0 ;
+    for (int i = 0 ; i < val.length; i++) {
+      sum += val[i];
     }
-    public static void printTriangle(int size) {
-        for (int i = 1; i <= size; i++) {
-            for (int j = size - i; j > 0; j--) {
-                System.out.print("");
-            }
-            for (int k = 1; k <= i; k++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-    }
-    public static void christmasTree(int size) {
-        for (int i = 1; i <= size; i++) {
-            for (int j = size - i; j > 0; j--) {
-                System.out.print(" ");
-            }
-            for (int k = 1; k <= 2 * i - 1; k++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-        size = size / 3;
-    }
+         
+    System.out.println( "Sum of all numbers = " + sum );   
+  }
 }
