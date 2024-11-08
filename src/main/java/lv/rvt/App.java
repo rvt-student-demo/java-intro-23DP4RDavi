653c879e2c332768dad2c166e7dc76249f70e7f0
Roberts <A230657rd@rvt.lv>
2024-11-08 13:54:09 +0200

    Product

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,25 +1,8 @@
 package lv.rvt;
 
-import java.util.*;
-
-class PersonalDetails
-{
-  public static void main ( String[] args )
-  {
-    int bdayavg = 0;
-    String longest = "";
-    Scanner scanner = new Scanner(System.in);
-    String[] names = {"","","","",""};
-    int[] bdays = {0,0,0,0,0};
-    for (int i = 0; i < 5; i++){
-      names[i] = String.valueOf(scanner.nextLine());
-      bdays[i] = Integer.valueOf(scanner.nextLine());
-      if (names[i].length() > longest.length()){
-        longest = names[i];
-      }
-      bdayavg += bdays[i];
-    }
-    System.out.println("Longest name:" + longest);
-    System.out.println("Average birth year:" + bdayavg / 5);
-   }
+public class App {
+  public static void main(String[] args) {
+    Product banana = new Product("Banana", 1.1, 13);
+    banana.printProduct();
+  }
 }
